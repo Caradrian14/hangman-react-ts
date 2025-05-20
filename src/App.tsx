534e60758cffce7bebc2b0019f8776ce1093d1d9
Adria <adria.jorda.m@gmail.com>
2025-05-20 21:47:15 +0200

    [add] letras y abecedario

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,10 @@ function App() {
 
       <HangmanDrawing />
       <HangmanWord />
-      <Keyboard />
+      <div style={{alignSelf: "stretch"}}>
+        <Keyboard />
+      </div>
+      
       
   </div>
   
